79fc702a203d9d80a4257e2a40f85e9b680bea7d
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-14 22:00:15 -0600

    Create YourDomain.IoC project

---
+++ YourDomain.IoC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using YourDomain.Controllers;
+using YourDomain.EFCore.Repositories;
+using YourDomain.Presenters;
+using YourDomain.UseCases;
+
+namespace YourDomain.IoC;
+
+public static class DependencyContainer
+{
+	public static IServiceCollection AddYourDomainServices(
+		this IServiceCollection services,
+		IConfiguration configuration,
+		string connectionStringName
+	)
+	{
+		services
+			.AddYourDomainEfCoreRepositories(configuration, connectionStringName)
+			.AddYourDomainUseCases()
+			.AddYourDomainPresenters()
+			.AddYourDomainControllers();
+
+		return services;
+	}
+}
