ae921a3bedaa323cf0d95b307be4efc068eb7390
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-14 22:31:05 -0600

    Create YourDomain.WebAPI project

---
+++ YourDomain.WebAPI/Controllers/WebApiController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using YourDomain.Controllers;
+using YourDomain.DTOs;
+
+namespace YourDomain.WebAPI.Controllers;
+
+[Route("api/Example")]
+[ApiController]
+public class WebApiController : ControllerBase
+{
+	private readonly YourDomain.Controllers.WithOutput.IMyController _controllerWithOutput;
+	private readonly YourDomain.Controllers.Empty.IMyController _controllerEmpty;
+	private readonly YourDomain.Controllers.WithInput.IMyController _controllerWithInput;
+	private readonly YourDomain.Controllers.WithBoth.IMyController _controllerWithBoth;
+
+	public WebApiController(YourDomain.Controllers.WithOutput.IMyController controllerWithOutput,
+							YourDomain.Controllers.Empty.IMyController controllerEmpty, 
+							YourDomain.Controllers.WithInput.IMyController controllerWithInput, 
+							YourDomain.Controllers.WithBoth.IMyController controllerWithBoth)
+	{
+		_controllerWithOutput = controllerWithOutput;
+		_controllerEmpty = controllerEmpty;
+		_controllerWithInput = controllerWithInput;
+		_controllerWithBoth = controllerWithBoth;
+	}
+
+	[HttpHead]
+	[Route("Empty")]
+	public async Task<IActionResult> GetEmpty()
+	{
+		await _controllerEmpty.RunUseCase();
+		return Ok();
+	}
+    
+	[HttpGet]
+	[Route("WithOutput")]
+	public async Task<IActionResult> GetOutputDto()
+	{
+		OutputDto outputDto = await _controllerWithOutput.RunUseCase();
+		return Ok(outputDto);
+	}
+    
+	[HttpPut]
+	[Route("WithInput")]
+	public async Task<IActionResult> GetInputDto(InputDto inputDto)
+	{
+		await _controllerWithInput.RunUseCase(inputDto);
+		return Ok();
+	}
+    
+	[HttpPost]
+	[Route("WithBoth")]
+	public async Task<IActionResult> GetBoth(InputDto inputDto)
+	{
+		OutputDto outputDto = await _controllerWithBoth.RunUseCase(inputDto);
+		return Ok(outputDto);
+	}
+}
---
+++ YourDomain.WebAPI/Program.cs
@@ -0,0 +1,36 @@
+using YourDomain.IoC;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddYourDomainServices(builder.Configuration, "YourEfDb");
+
+WebApplication app = builder.Build();
+
+app.UseCors(policy =>
+{
+	policy.AllowAnyHeader()
+		.AllowAnyMethod()
+		.AllowAnyOrigin();
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+		options.RoutePrefix = string.Empty;
+	});
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
