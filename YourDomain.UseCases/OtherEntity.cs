9ac6985580796e565220dfc6a1081d039fbf53cd
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:54:48 -0600

    Add OtherEntity.cs in UseCases

---
+++ YourDomain.UseCases/OtherEntity.cs
@@ -0,0 +1,13 @@
+﻿using YourDomain.Model.Entities;
+
+namespace YourDomain.UseCases;
+
+public class OtherEntity
+{
+	public string Name { get; set; }
+
+	public static OtherEntity MapFrom(Entity source)
+	{
+		return new OtherEntity() { Name = source.Name };
+	}
+}
