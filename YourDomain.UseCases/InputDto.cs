6eb63c0d557817b2df332925faabfa067dbf1fea
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:56:30 -0600

    Refactor InputDto to include PagingParams

--- YourDomain.UseCases/InputDto.cs
+++ YourDomain.UseCases/InputDto.cs
@@ -1,6 +1,14 @@
-﻿namespace YourDomain.UseCases;
+﻿using SeedWork;
+
+namespace YourDomain.UseCases;
 
 public class InputDto
 {
+	public PagingParams PagingParams { get; set; }
 	public string Data { get; set; }
+	
+	public InputDto()
+	{
+		PagingParams = new PagingParams();
+	}
 }
