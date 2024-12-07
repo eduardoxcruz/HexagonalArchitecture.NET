617bd4f9fdb4edc1bc80243617d41486d9e8aee6
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:13:30 -0600

    Add UnitOfWork.cs in EfCore.DataContext

---
+++ YourDomain.EFCore.DataContext/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+using SeedWork;
+
+namespace YourDomain.EFCore.DataContext;
+
+public class UnitOfWork: IUnitOfWork, IDisposable
+{
+	private readonly EfDbContext _context;
+	private Hashtable _repositories;
+
+	public UnitOfWork(EfDbContext context)
+	{
+		_context = context;
+		_repositories = new Hashtable();
+	}
+
+	public async ValueTask<int> SaveChangesAsync()
+	{
+		return await _context.SaveChangesAsync();
+	}
+
+	public IRepository<TEntity> Repository<TEntity>() where TEntity : class
+	{
+		string type = typeof(TEntity).Name;
+
+		if (_repositories.ContainsKey(type)) return ((IRepository<TEntity>)_repositories[type]!)!;
+            
+		Type repositoryType = typeof(Repository<>);
+
+		object repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context)!;
+
+		_repositories.Add(type, repositoryInstance);
+
+		return ((IRepository<TEntity>)_repositories[type]!)!;
+	}
+
+	public void Dispose()
+	{
+		_context.Dispose();
+	}
+}
