943358a8e6f88b588ad44d705214cf39f11487d3
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:57:01 -0600

    Refactor WithBoth.UseCase to include UnitOfWork, Specification and PagedList

--- YourDomain.UseCases/WithBoth/UseCase.cs
+++ YourDomain.UseCases/WithBoth/UseCase.cs
@@ -2,28 +2,43 @@
 
 using YourDomain.Model.Entities;
 using YourDomain.Model.Repositories;
+using YourDomain.Model.Services.Specifications;
 
 namespace YourDomain.UseCases.WithBoth;
 
-public interface IUseCaseInputPort : IPort<InputDto> { }
+public interface IUseCaseInputPort : IPort<InputDto>
+{
+}
 
-public interface IUseCaseOutputPort : IPort<OutputDto> { }
+public interface IUseCaseOutputPort : IPort<OutputDto>
+{
+}
 
 public class UseCase : IUseCaseInputPort
 {
 	private readonly IUseCaseOutputPort _outputPort;
-	private readonly IEntityRepository _repository;
+	private readonly IUnitOfWork _unitOfWork;
 
-	public UseCase(IUseCaseOutputPort outputPort, IEntityRepository repository)
+	public UseCase(IUseCaseOutputPort outputPort, IUnitOfWork unitOfWork)
 	{
 		_outputPort = outputPort;
-		_repository = repository;
+		_unitOfWork = unitOfWork;
 	}
 
 	public async ValueTask Handle(InputDto dto)
 	{
-		string outData = await _repository.Update(new Entity($"Use Case.WithBoth with input dto: {dto.Data}"));
-		OutputDto outputDto = new(outData);
+		EntityWithNameSpecification specification = new(dto.Data, dto.PagingParams);
+		PagedList<Entity> entitiesFromDb = await _unitOfWork.Repository<Entity>().FindAsync(specification);
+		
+		// You can convert the entities to another just by doing:
+		PagedList<OtherEntity> entities = new(
+			entitiesFromDb.Select(OtherEntity.MapFrom).ToList(),
+			entitiesFromDb.TotalCount,
+			entitiesFromDb.CurrentPage,
+			entitiesFromDb.PageSize);
+		// And then pass to outputDto
+		
+		OutputDto outputDto = new(entitiesFromDb);
 		await _outputPort.Handle(outputDto);
 	}
 }
