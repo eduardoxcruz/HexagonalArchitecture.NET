1dac5e91d11f9df93bcd9ce498b712a6b1faf254
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:26:59 -0600

    Add EntityWithNameSpecification.cs

---
+++ YourDomain.Model/Services/Specifications/EntityWithNameSpecification.cs
@@ -0,0 +1,14 @@
+﻿using SeedWork;
+
+using YourDomain.Model.Entities;
+
+namespace YourDomain.Model.Services.Specifications;
+
+public sealed class EntityWithNameSpecification : BaseSpecification<Entity>
+{
+	public EntityWithNameSpecification(string? name, PagingParams pagingParams) : 
+		base(entity => (entity.Name.Equals(name)))
+	{
+		ApplyPaging(pagingParams.PageNumber, pagingParams.PageSize);
+	}
+}
