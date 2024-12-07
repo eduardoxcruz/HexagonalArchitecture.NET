cf4af182313baddb2effe958fdc3513a07e5aaae
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 15:57:13 -0600

    Add ISpecification.cs interface

---
+++ SeedWork/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace SeedWork;
+
+public interface ISpecification<T>
+{
+	int PageNumber { get; }
+	int PageSize { get; }
+	bool IsPagingEnabled { get; }
+	List<string> IncludeStrings { get; }
+	List<Expression<Func<T, object>>> Includes { get; }
+	Expression<Func<T, bool>>? Criteria { get; }
+	Expression<Func<T, object>>? GroupBy { get; }
+	Expression<Func<T, object>>? OrderBy { get; }
+	Expression<Func<T, object>>? OrderByDescending { get; }
+}
