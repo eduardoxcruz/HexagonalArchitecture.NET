cd1caeccb8819fc0b16897ea018e9d2fcd6a55c6
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-07 16:13:01 -0600

    Add Repository.cs to EFCore.DataContext

---
+++ YourDomain.EFCore.DataContext/Repository.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq.Expressions;
+
+using Microsoft.EntityFrameworkCore;
+
+using SeedWork;
+
+namespace YourDomain.EFCore.DataContext;
+
+public class Repository<TEntity>(DbContext context) : IRepository<TEntity> where TEntity : class
+{
+	public async ValueTask<TEntity?> FindByIdAsync(object id)
+	{
+		return await context.Set<TEntity>().FindAsync(id);
+	}
+
+	public async ValueTask<PagedList<TEntity>> FindAsync(ISpecification<TEntity>? specification = null)
+	{
+		IQueryable<TEntity> query = await ApplySpecification(specification);
+		int count = await query.CountAsync();
+		
+		if (specification?.IsPagingEnabled == true)
+		{
+			query = query
+				.Skip((specification.PageNumber - 1) * specification.PageSize)
+				.Take(specification.PageSize);
+		}
+
+		List<TEntity> items = await query.AsNoTracking().ToListAsync();
+		
+		return new PagedList<TEntity>(items, count, specification?.PageNumber ?? 1, specification?.PageSize ?? count);
+	}
+
+	public async ValueTask AddAsync(TEntity entity)
+	{
+		await context.Set<TEntity>().AddAsync(entity);
+	}
+
+	public async ValueTask AddRangeAsync(IEnumerable<TEntity> entities)
+	{
+		await context.Set<TEntity>().AddRangeAsync(entities);
+	}
+
+	public ValueTask RemoveAsync(TEntity entity)
+	{
+		context.Set<TEntity>().Remove(entity);
+		return ValueTask.CompletedTask;
+	}
+
+	public ValueTask RemoveRangeAsync(IEnumerable<TEntity> entities)
+	{
+		context.Set<TEntity>().RemoveRange(entities);
+		return ValueTask.CompletedTask;
+	}
+
+	public ValueTask UpdateAsync(TEntity entity)
+	{
+		context.Set<TEntity>().Attach(entity);
+		context.Entry(entity).State = EntityState.Modified;
+		return ValueTask.CompletedTask;
+	}
+
+	public ValueTask UpdateRangeAsync(IEnumerable<TEntity> entities)
+	{
+		context.Set<TEntity>().AttachRange(entities);
+		
+		foreach (TEntity entity in entities)
+		{
+			context.Entry(entity).State = EntityState.Modified;
+		}
+		
+		return ValueTask.CompletedTask;
+	}
+
+	public async ValueTask<bool> ContainsAsync(ISpecification<TEntity>? specification = null)
+	{
+		return await CountAsync(specification) > 0;
+	}
+
+	public async ValueTask<bool> ContainsAsync(Expression<Func<TEntity, bool>> predicate)
+	{
+		return await CountAsync(predicate) > 0;
+	}
+
+	public async ValueTask<int> CountAsync(ISpecification<TEntity>? specification = null)
+	{
+		return await (await ApplySpecification(specification)).CountAsync();
+	}
+
+	public async ValueTask<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+	{
+		return await context.Set<TEntity>().Where(predicate).CountAsync();
+	}
+	
+	private ValueTask<IQueryable<TEntity>> ApplySpecification(ISpecification<TEntity>? spec)
+	{
+		return SpecificationEvaluator<TEntity>.GetQuery(context.Set<TEntity>().AsQueryable(), spec);
+	}
+}
