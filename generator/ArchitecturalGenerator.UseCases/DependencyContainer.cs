bf02a39212dc131e676ca3ac00b646a0b8082e49
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-10-20 03:08:10 -0600

    Add new folders and configure IoC for project templates use case
    - Added support for new folder structure related to project templates.
    - Registered `AddProjectTemplates.UseCase` in IoC with required domain port.

--- generator/ArchitecturalGenerator.UseCases/DependencyContainer.cs
+++ generator/ArchitecturalGenerator.UseCases/DependencyContainer.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
+using SeedWork;
+
 namespace ArchitecturalGenerator.UseCases;
 
 public static class DependencyContainer
 {
 	public static IServiceCollection AddYourDomainUseCases(this IServiceCollection services)
 	{
+		services.AddScoped<IDomainPort<EmptyDto, AddProjectTemplates.ProjectTemplatesPathDto>, AddProjectTemplates.UseCase>();
+		
 		return services;
 	}
 }
